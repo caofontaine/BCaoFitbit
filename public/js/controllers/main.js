d20b4f482e2c2bbf2c530dfc4dc5672dd4e09985
Ben Cao <benjamin.cao88@gmail.com>
2017-02-22 16:04:25 -0500

    Hit/Miss Pie Chart
    Implementation of a pie chart showing number of times hit or missed on
    Fitbit step goal.

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,6 +1,6 @@
 /*Grab the data from the API call and format it in Google Charts.*/
 
-angular.module('BCaoFitbitController', []).controller('mainController', function($scope, $http, BCaoFitbits) {
+angular.module('BCaoFitbitController', []).controller('allData', function($scope, $http, BCaoFitbits) {
 
     BCaoFitbits.get().then(function(data) { //success() deprecated in 1.6.1
       var fitbitData;
@@ -38,10 +38,63 @@ angular.module('BCaoFitbitController', []).controller('mainController', function
           vAxis: {
             title: 'Steps'
           },
-          backgroundColor: '#f1f8e9'
+          backgroundColor: '#f1f8e9',
+          title: 'Lifetime Data'
         };
 
-        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        var chart = new google.visualization.LineChart(document.getElementById('all_data_div'));
+        chart.draw(stepData, options);
+        
+      }
+      
+      // Makes chart responsive to resize of browser.
+      $(window).resize(function(){
+        drawChart();
+      });
+    });
+}).controller('stepGoals', function($scope, $http, BCaoFitbits) {
+
+    BCaoFitbits.get().then(function(data) { //success() deprecated in 1.6.1
+      var fitbitData;
+      fitbitData = data;
+			
+      google.charts.load('current', {packages: ['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var stepData = new google.visualization.DataTable();
+        stepData.addColumn('string', 'Goal');
+        stepData.addColumn('number', 'NumGoal');
+        
+        var hit = 0, miss = 0;
+				
+        //console.log(fitbitData.length);
+				
+        for(var i = 0; i < fitbitData.data.length; i++) {
+          // Data has number of steps as a string (since it has commas). Convert it to integer.
+          // Must get rid of comma while doing so, or will error.
+          var strToInt = parseInt(fitbitData.data[i]['Steps'].replace(/\,/g,''));
+
+          if(strToInt >= 7000) {
+            hit++;
+          }
+          else {
+            miss++;
+          }
+        }
+
+        stepData.addRow(['Hit', hit]);
+        stepData.addRow(['Miss', miss]);
+
+        var options = {
+          title: 'Times Hit Goal Steps (7,000+)',
+          slices: {
+            0: { color: 'green' },
+            1: { color: 'red' }
+          }
+        };
+
+        var chart = new google.visualization.PieChart(document.getElementById('step_goals_div'));
         chart.draw(stepData, options);
         
       }
--- public/js/core.js
+++ public/js/core.js
@@ -1,3 +1,4 @@
 /*List of controller and service used for front end interaction.*/
 
-var fitbitPOC = angular.module('BCaoFitbit', ['BCaoFitbitController', 'BCaoFitbitService']);
+var allData = angular.module('BCaoFitbit', ['BCaoFitbitController', 'BCaoFitbitService']);
+var stepGoals = angular.module('BCaoFitbit', ['BCaoFitbitController', 'BCaoFitbitService']);
