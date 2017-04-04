6f047560a0d1e56b935bc8d7e018edd9fb712d10
Ben Cao <benjamin.cao88@gmail.com>
2017-04-04 13:15:15 -0400

    Update updateDB.pl
    Included command to convert Date field to an ISO-Date type.
    
    Why? The Date field exported from Fitbit is a string, therefore, it does
    not sort properly by date if it is a string. By converting to an
    ISO-Date type proper for Fitbit, it will sort properly by date.

--- scripts/updateDB.pl
+++ scripts/updateDB.pl
@@ -2,4 +2,10 @@
 
 use 5.014;
 
-exec ('mongoimport --db BCaoFitbit --collection fitbitdata --type csv --drop --file C:/Node/BCaoFitbit/data/BCaoFitbit.csv --headerline');
+#Import data from .csv file.
+system ('mongoimport --db BCaoFitbit --collection fitbitdata --type csv --drop --file C:/Node/BCaoFitbit/data/BCaoFitbit.csv --headerline');
+
+print "\nConverting Date to ISO-Date type:";
+
+# To convert date from MM/DD/YYYY to MongoDB Date type:
+system ('mongo BCaoFitbit --eval "db.fitbitdata.find().forEach(function(doc) { doc.Date = new Date(doc.Date); db.fitbitdata.save(doc);})"');
